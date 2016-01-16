9e42ec8b92838460cb95b8bf83f8398ccc984bc2
bjarneo <bjarne.oeverli@gmail.com>
2016-01-16 13:03:04 +0100

    remove is function and string since I'm using lodash

--- src/is-function.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function isFunction(func) {
-    if (Object.prototype.toString.call(func) !== '[object Function]') {
-        return false;
-    }
-
-    return true;
-};
--- src/is-string.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function isString(str) {
-    if (Object.prototype.toString.call(str) !== '[object String]') {
-        return false;
-    }
-
-    return true;
-};
